7ea5851c8a4aa5a0467a31512c2d8cb87a604053
mikecalendo <contact@michaelcalendo.com>
2022-02-16 00:23:27 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+// Copyright IBM Corp. 2018,2020. All Rights Reserved.
+// Node module: @loopback/example-rpc-server
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {ApplicationConfig, MyApplication} from './application';
+
+export * from './application';
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new MyApplication(options);
+
+  await app.start();
+  console.log(`Server is running on port ${app.options.port}`);
+  return app;
+}
+
+if (require.main === module) {
+  // Run the application
+  main().catch(err => {
+    console.error('Cannot start the application.', err);
+    process.exit(1);
+  });
+}
---
+++ src/rpc.server.ts
@@ -0,0 +1,53 @@
+// Copyright IBM Corp. 2018,2020. All Rights Reserved.
+// Node module: @loopback/example-rpc-server
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {
+  Application,
+  Context,
+  CoreBindings,
+  inject,
+  Server,
+} from '@loopback/core';
+import {once} from 'events';
+import express from 'express';
+import http from 'http';
+import {rpcRouter} from './rpc.router';
+
+export class RPCServer extends Context implements Server {
+  private _listening = false;
+  _server: http.Server;
+  expressServer: express.Application;
+
+  constructor(
+    @inject(CoreBindings.APPLICATION_INSTANCE) public app?: Application,
+    @inject('rpcServer.config') public config?: RPCServerConfig,
+  ) {
+    super(app);
+    this.config = config ?? {};
+    this.expressServer = express();
+    rpcRouter(this);
+  }
+
+  get listening() {
+    return this._listening;
+  }
+
+  async start(): Promise<void> {
+    this._server = this.expressServer.listen(this.config?.port ?? 3000);
+    this._listening = true;
+    await once(this._server, 'listening');
+  }
+  async stop(): Promise<void> {
+    this._server.close();
+    this._listening = false;
+    await once(this._server, 'close');
+  }
+}
+
+export type RPCServerConfig = {
+  port?: number;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any;
+};
